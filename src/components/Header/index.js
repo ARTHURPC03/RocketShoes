23f4d231371b4cf82789358358a5f7ef96a5cb25
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-03-24 21:52:29 -0300

    tentando adicionar Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,13 @@
 import React from 'react'
 
-import { createStackNavigator } from '@react-navigation/stack'
 import { Wrapper, Container, Logo } from './styles'
-
-const Stack = createStackNavigator()
 
 function Header() {
   return (
     <Wrapper>
-      <Stack.Screen
-        name="Header"
-        component={Header}
-        options={{
-          headerTitle: <Logo style={{ width: 50, height: 50 }} />,
-        }}
-      />
-      <Container />
+      <Container>
+        <Logo />
+      </Container>
     </Wrapper>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,28 @@ import Header from './components/Header'
 const Stack = createStackNavigator()
 
 export default function Routes() {
+  const StackNavigatorScreenOptions = {
+    headerBackTitleVisible: false,
+    headerLeftContainerStyle: {
+      marginLeft: 10,
+    },
+    headerRightContainerStyle: {
+      marginRight: 10,
+    },
+    headerStyle: {
+      backgroundColor: '#7159c1',
+    },
+    headerTintColor: '#fff',
+  }
+
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen
+        name="Home"
+        options={{ header: Header }}
+        screenOptions={StackNavigatorScreenOptions}
+        component={Home}
+      />
       <Stack.Screen name="Cart" component={Cart} />
     </Stack.Navigator>
   )
